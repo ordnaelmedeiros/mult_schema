cd18a3005c556fe54847a5921ecdeea46b7e2e73
Anderson Oliveira <andherrson@hotmail.com>
2015-11-12 20:13:02 -0200

    app
    app

--- MultSchema/WebContent/app/js/app.module.js
+++ MultSchema/WebContent/app/js/app.module.js
@@ -1,53 +1,58 @@
-var app = angular.module('app', [
-/*Angular Modules*/                    
-	'ngMaterial',
-	'ngMessages',
-	'ngRoute'
-]);
+(function() {
+	'use strict';
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav, $rootScope, $location){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
-  
-  $scope.isOpen = false;
-  $scope.demo = {
-    isOpen: false,
-    count: 50,
-    selectedAlignment: 'md-right'
-  };
-  
-  $scope.menu = [
-     {
-       title: 'Home',
-       rota:'home',
-       icon: 'dashboard',
-       tipo:'link'
-     },
-     {
-       title: 'Sobre',
-       rota:'sobre',
-       icon: 'dashboard',
-       tipo:'link'
-     },
-     {
-         title: 'Tela 1',
-         rota:'tela1',
-         icon: 'dashboard',
-         tipo:'link'
-       }
-    ]
-  
-  $scope.iconFavorite = "app/img/icons/ic_favorite_border_white_48px.svg";
-  
-  $scope.trocaCoracao = function() {
+	angular
+		.module('app', [
+		/*Angular Modules*/                    
+			'ngMaterial',
+			'ngMessages',
+			'ngRoute'
+		])
+		.controller('AppCtrl', ['$scope', '$mdSidenav', 
+		function($scope, $mdSidenav, $rootScope, $location){
+			$scope.toggleSidenav = function(menuId) {
+		    $mdSidenav(menuId).toggle();
+		};
 	  
-	  if ($scope.iconFavorite=="app/img/icons/ic_favorite_white_48px.svg") {
-		  $scope.iconFavorite = "app/img/icons/ic_favorite_border_white_48px.svg";
-	  } else {
-		  $scope.iconFavorite = "app/img/icons/ic_favorite_white_48px.svg";
-	  }
+		 $scope.isOpen = false;
+		 $scope.demo = {
+				 isOpen: false,
+				 count: 50,
+				 selectedAlignment: 'md-right'
+		  };
 	  
-  } 
-  
-}]);
+		 $scope.menu = [
+	     {
+	       title: 'Home',
+	       rota:'home',
+	       icon: 'dashboard',
+	       tipo:'link'
+	     },
+	     {
+	       title: 'Sobre',
+	       rota:'sobre',
+	       icon: 'dashboard',
+	       tipo:'link'
+	     },
+	     {
+	         title: 'Tela 1',
+	         rota:'tela1',
+	         icon: 'dashboard',
+	         tipo:'link'
+	       }
+	    ]
+	  
+		 $scope.iconFavorite = "app/img/icons/ic_favorite_border_white_48px.svg";
+	  
+		 $scope.trocaCoracao = function() {
+		  
+			 if ($scope.iconFavorite=="app/img/icons/ic_favorite_white_48px.svg") {
+				 $scope.iconFavorite = "app/img/icons/ic_favorite_border_white_48px.svg";
+			 } else {
+				 $scope.iconFavorite = "app/img/icons/ic_favorite_white_48px.svg";
+			 }
+			 
+		  } 
+	}]);
+})();
+
